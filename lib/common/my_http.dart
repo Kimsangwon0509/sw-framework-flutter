973669f6d1eef6766aa85dd74270fc090cc879ae
KIM_SANGWON04 <kim_sangwon04@elandinnople.com>
2024-06-11 15:19:16 +0900

    http factory 추가

--- lib/common/my_http.dart
+++ lib/common/my_http.dart
@@ -1,1 +1,9 @@
-class MyHttp {}
+class MyHttp {
+  static final MyHttp _instance = MyHttp._internal();
+  factory MyHttp() {
+    return _instance;
+  }
+  MyHttp._internal();
+
+  void _call() {}
+}
